1a7737c36c7e54518cfa20fbb292b13633e5c79b
blesinto <bomerenma@gmail.com>
2024-04-21 22:20:11 +0100

    four commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => (
   <div className='ps-[5rem] pe-[5rem] x:pe-[2rem] x:ps-[2rem] x:flex-col justify-between items-center flex mt-[5rem] '>
     <div className="flex flex-col x:text-center x:mb-[3rem]">
       <h5>Hi I am</h5>
-      <h2>Mahmood Fazile</h2>
+      <h2>Ekemini Udongwo</h2>
       <h1>Backend Developer</h1>
     </div>
     {/* image  */}
